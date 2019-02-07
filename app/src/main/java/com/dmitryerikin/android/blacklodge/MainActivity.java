31920433cca50e673b39ee6b2bf9b5a4ea2e7cf9
Dmitry Erikin <dmitryerikin@gmail.com>
2019-02-07 21:22:29 +0300

    Определил цвета приложения. Создал иконки для кнопок из заготовок для иконок меню. Создал макет активности.

--- app/src/main/java/com/dmitryerikin/android/blacklodge/MainActivity.java
+++ app/src/main/java/com/dmitryerikin/android/blacklodge/MainActivity.java
@@ -1,7 +1,13 @@
 package com.dmitryerikin.android.blacklodge;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void shortToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
